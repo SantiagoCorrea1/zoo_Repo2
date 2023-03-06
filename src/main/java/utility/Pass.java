332eea0200d9b5b417c77a1f534e82e817a69b70
SANTIAGO <SANTIAGO@192.168.1.50>
2023-03-05 20:34:21 -0500

    -agregé las clases faltantes (animales domésticos la dejo en blanco porque creo que la eliminaremos)

---
+++ src/main/java/utility/Pass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utility;
+
+/**
+ *
+ * @author SANTIAGO
+ */
+public class Pass {
+    private double price;
+    private String code;
+    private int cant_times;
+}
---
+++ src/main/java/utility/Ticket.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utility;
+
+/**
+ *
+ * @author SANTIAGO
+ */
+public class Ticket {
+    private double price;
+    private String code;
+    private String age;
+    private int age_restriction;
+    
+}
